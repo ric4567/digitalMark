1d39a7dd0456c06aa806b0625f44c235d201a275
Ricardo Reinert <ricardorkarnopp@gmail.com>
2023-03-03 20:09:33 -0300

    feat: adicionadas entidades

---
+++ DigitalMark.Domain/Entities/Client.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalMark.Domain.Entities
+{
+    public class Client
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+    }
+}
---
+++ DigitalMark.Domain/Entities/Project.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalMark.Domain.Entities
+{
+    public class Project
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public Client Client { get; private set; }
+    }
+}
